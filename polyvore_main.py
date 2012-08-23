ed74b9f9748cb9b8bc9abcbb8358438e11abeb9a
Jamie G <jamie.giedinghagen@gmail.com>
2012-08-23 00:01:56 -0700

    init

--- polyvore_main.py
+++ polyvore_main.py
@@ -1,6 +1,7 @@
 from random import choice
 from flask import Flask, render_template, request
 import polyvore
+import os
 app = Flask(__name__)
 import logging
 import requests
@@ -120,4 +121,5 @@ def search():
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+   port = int(os.environ.get('PORT', 5000))
+   app.run(host='0.0.0.0', port=port)
